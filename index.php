5a980da1357222c6778488abe32052355024a7d1
root <root@edin-All-Series.(none)>
2015-12-25 12:02:50 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>Klix</title>
+	</head>
+	<body>
+	<nav>
+
+
+	</nav>
+	<header>
+			
+
+
+	</header>
+	<section>
+
+
+
+
+	</section>	
+
+
+
+	<footer>
+		
+
+
+
+	</footer>
+
+
+	</body>
+</html>
