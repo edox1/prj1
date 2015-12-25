0abd4002f48946fd0b3807368e1c0f5a0e24b8d7
sanel <sanel@mail.com>
2015-12-25 16:48:42 +0100

    Modified navbar with css

--- includes/navigation.php
+++ includes/navigation.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="../css/style.css" >
+<script src="../js/toggleactive.js" type="text/javascript"></script>
 <div class="navbar navbar-default" role="navigation">
   <div class="container-fluid col-md-8 col-md-offset-2">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -19,37 +20,22 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+        <li class="active"><a href="#" data-toggle="tab"><div class="kocka bg_vijesti"></div>Vijesti <span class="sr-only">(current)</span></a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_biznis"></div>Biznis</a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_sport"></div>Sport</a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_magazin"></div>Magazin</a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_lifestyle"></div>Lifestyle</a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_scitech"></div>Scitech</a></li>
+        <li><a href="#" data-toggle="tab"><div class="kocka bg_auto"></div>Auto</a></li>
+        
+        
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+     
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+        <li><a href="#" ><span class="glyphicon glyphicon-user"></span> Login</a></li>
+        
           </ul>
         </li>
       </ul>
