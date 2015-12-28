5703fd00f5faa8abcced7ec36d105e89a2705905
sanel <sanel@mail.com>
2015-12-28 15:53:51 +0100

    Added and styled modal login form

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="../css/style.css" >
 <script src="../js/toggleactive.js" type="text/javascript"></script>
@@ -34,7 +35,7 @@
       </ul>
      
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="../login.php" ><span class="glyphicon glyphicon-user"></span> Login</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-user"></span> Login</a></li>
         
           </ul>
         </li>
@@ -42,3 +43,58 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+<!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Prijava</h4>
+        </div>
+        <div class="modal-body">
+         
+               <form role="form">
+                    <div class="form-group formshort">
+                      <label for="email">Korisnicko ime ili e-mail</label>
+                      <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    </div>
+                    <div class="form-group formshort">
+                      <label for="pwd">Lozinka</label>
+                      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    </div>
+                    <div class="checkbox formshort">
+                      <label><input type="checkbox"> Remember me</label>
+                    </div>
+
+                    <!-- Button -->
+                    <div class="form-group formshort">
+                      <label class= "control-label" for="submit"></label>
+                      <div class="prijavadesno">
+                        <button id="submit" name="submit" class="btn btn-primary">Prijavi se</button>
+                      </div>
+                    </div>
+                    <div class="form-group formshort">
+                    <div class="ili-separator col-xs-12 no-padding">
+                        <div class="lajna"></div>
+                        <div class="krug-ili">ili</div>
+                    </div>
+                    </div>
+                    <p>&nbsp;</p>
+                    <div class="col-xs-12 buttonpad">
+                        <a href="#" ><button type="button" class="btn btn-info btn-lg btn-block nodecor">Registruj se</button>
+                        </a>
+                    </div>
+                    <p>&nbsp;</p>
+                    </form>
+           
+               
+        </div>
+       
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
