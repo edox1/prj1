e1daff1846f631e2ab607a323b5857f542a0ffd4
edin <edo_hal@hotmail.com>
2015-12-30 14:07:34 +0100

    izmjena u index.html

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
  <meta charset="UTF-8"> 
  <link rel="stylesheet" type="text/css" href="css/style2.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 </head>
 <body>
@@ -10,91 +11,96 @@
 
 <?php include 'includes/navigation.php'; ?>
 
-
-
 <div class="container">
-		<section class="container">  
+<div class="row" >
+	<div class="col-md-9">
+		<section class="container1">  
 			<article class="news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/1.jpg" />
 				<header class="news-header-bih">Podignuta optužnica u predmetu "Meso", država oštećena za 13,5 miliona KM</header>
 				<section class="news-item">Optuženi se terete da su, u periodu od 2007. do 2012. godine, 
 				djelujući kao povezana grupa fizičkih osoba i pravnih osoba (preduzeća), 
 				počinili porezne utaje i neplaćanje poreza...
-				 </section>
+				</section>
+				</a>
 			</article>  
 			<article class="news-big news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/bakir.jpg" />
 				<header class="news-header-bih">Izetbegović o spajanju kantona: Logično je da se ujedine Sarajevo, Zenica i Goražde</header>
 				<section class="news-item">Što se tiče preustroja FBiH, Bakir Izetbegović je na današnjoj novogodišnjoj konferenciji SDA kazao kako
 				 uskoro očekuje prijedloge HDZ-a BiH o preuređenju Mostara...
 				 </section>
+				 </a>
 			</article>  
 			<article class="news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/3.jpg" />
 				<header class="news-header-bih">Maskirani razbojnici u Lukavcu opljačkali banku i odnijeli veću količinu novca</header>
 				<section class="news-item">"U Lukavcu, u ulici Armije RBiH jutros u 8:10 sati izvršeno je razbojništvo, 
 				kada su dvije nepoznate osobe sa fantomkama na glavi i automatskim oružjem upale 
 				u banku i otuđile veću količinu novca", izjavio je ...
 				</section>
+				</a>
 			</article> 
 
 		</section>
-		<section class="container">  
+		<section class="container1">  
 			
 			<article class="news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/4.jpg" />
 				<header class="news-header-bih">Skupština KS usvojila Zakon o unutrašnjim poslovima: Finansijska samostalnost i depolitizacija Uprave policije</header>
-			
+				</a>
 			</article>  
 			<article class="news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/5.jpg" />
 				<header class="news-header-bih">Zurovac: Istražni organi se ni danas ne bave sadržajem snimka Željke Cvijanović</header>
-				
+				</a>
 			</article>  
 			<article class="news-bih">
 				<div class="header-line">
 					<a href="#">BIH</a>
 				</div>
+				<a href="#">
 				<img src="images/6.jpg" />
 				<header class="news-header-bih">Ana Babić iskritikovala kolege koje su napustile sjednicu Skupštine KS</header>
-				
+				</a>
 			</article> 
 			<article class="news-edukacija">
 				<div class="header-line">
 					<a href="#">EDUKACIJA</a>
 				</div>
+				<a href="#">
 				<img src="images/7.jpg" />
 				<header class="news-header-edukacija">Dvanaest izreka koje vas mogu inspirisati u 2016. godini</header>
-				
+				</a>
 			</article> 
 
 		</section>
-</div>
-<div class="container">
-			<article class="news-edukacija">
-				<div class="header-line">
-					<a href="#">EDUKACIJA</a>
-				</div>
-				<img src="images/7.jpg" />
-				<header class="news-header-edukacija">Dvanaest izreka koje vas mogu inspirisati u 2016. godini</header>
-				
-			</article> 
+	</div>
+	<div class="col-md-3" style="background-color:#40AFEE; height:800px">
+			sdfgsdgdfg dsfsdfds dfgfdg dfsgfdssssssssssssg fdsgdfgdsf                 dfsgfdgsdfgdfgfd        dfsgdfg dfgdsgdfgdfgdfgdf
 
+	</div>
+</div>
 </div>
 
 
